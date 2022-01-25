cb0c7431c4ea510ff0fc0959aefbbe9cbf6521b6
Nikita Sidiropulo <sidiropulonikita04@gmail.com>
2022-01-25 12:43:52 +0300

    присабачил меню-бургер

--- app/js/main.js
+++ app/js/main.js
@@ -12,9 +12,18 @@ $('.main-slider__items').slick({
 				autoplay: true,
 				autoplaySpeed: 6000,
 				infinite: true,
-				arrows: false,
 				dots: true
 			}
 		}
 	]
 });
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock');
+		menuBody.classList.toggle('_active');
+		iconMenu.classList.toggle('_active');
+	});
+}
